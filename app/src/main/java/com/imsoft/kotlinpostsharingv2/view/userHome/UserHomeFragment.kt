05b6567c5f99a2e57ef4d6b6a833e906d7e3e27d
Muarem <s.muarem10@gmail.com>
2024-02-11 21:28:42 +0400

    get and read real time data for user home

--- app/src/main/java/com/imsoft/kotlinpostsharingv2/view/userHome/UserHomeFragment.kt
+++ app/src/main/java/com/imsoft/kotlinpostsharingv2/view/userHome/UserHomeFragment.kt
@@ -8,12 +8,19 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import com.imsoft.kotlinpostsharingv2.R
 import com.imsoft.kotlinpostsharingv2.databinding.FragmentUserHomeBinding
+import com.imsoft.kotlinpostsharingv2.model.Posts
 import com.imsoft.kotlinpostsharingv2.view.GetPostsFragmentDirections
 
 @Suppress("DEPRECATION")
@@ -21,6 +28,9 @@ class UserHomeFragment : Fragment() {
 
     private lateinit var binding: FragmentUserHomeBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var storage: FirebaseStorage
+    private lateinit var postList: ArrayList<Posts>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +50,12 @@ class UserHomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         auth = Firebase.auth
+        firestore = Firebase.firestore
+        storage = Firebase.storage
+
+        postList = ArrayList<Posts>()
+
+        getUserPosts()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -70,6 +86,41 @@ class UserHomeFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
+
+    private fun getUserPosts() {
+
+        val uid = auth.currentUser?.uid
+
+        firestore.collection("Posts")
+            .whereEqualTo("userId", uid)
+            .orderBy("date", Query.Direction.DESCENDING)
+            .addSnapshotListener { value, error ->
+                if (error == null) {
+                    if (value != null && !(value.isEmpty)) {
+                        val documents = value.documents
+
+                        postList.clear()
+
+                        for (document in documents) {
+                            val username = document["userName"] as String
+                            val comment = document["userComment"] as String
+                            val downloadUrl = document["downloadUrl"] as String
+
+                            val post = Posts(username, comment, downloadUrl)
+
+                            println("username from user home $username")
+                            postList.add(post)
+                        }
+
+//                        postAdapter.notifyDataSetChanged()
+                    }
+                } else {
+                    Toast.makeText(requireContext(), error.localizedMessage, Toast.LENGTH_LONG).show()
+                    println(error.localizedMessage)
+                }
+            }
+
+    }
 
 
 }
